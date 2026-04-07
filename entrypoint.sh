8f3b7418c7ab0a1f8ed31e1241c2fc1a9da1a1e4
ThuNa255 <nguyenna25504@gmail.com>
2026-04-08 01:59:27 +0700

    oke

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 set -e
 
-
+# Xóa cache
 php artisan config:clear
+php artisan cache:clear
 
-php artisan migrate --force || true
+# Xóa sạch bảng cũ và tạo lại mới hoàn toàn (Tránh lỗi Duplicate)
+php artisan migrate:fresh --force
 
+# Khởi động Apache
 exec apache2-foreground
