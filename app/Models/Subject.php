489a196a57af5f026bbb4bb0da72316406be7778
mab30367@gmail.com <nguyenna25504@gmail.com>
2026-04-18 00:48:28 +0700

    hope

--- app/Models/Subject.php
+++ app/Models/Subject.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Subject extends Model
 {
+    protected $table = 'subjects';
     protected $fillable = ['name', 'description'];
 
     public function exams()
